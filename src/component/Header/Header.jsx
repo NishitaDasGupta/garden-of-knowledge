ac18930d30a0a83c246f984805bfd3792be34299
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-03-31 17:04:11 +0600

    mobile responsive

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <div className='flex justify-between items-center'>
-            <h2 className="text-4xl font-bold "
+            <div className='md:flex justify-between items-center'>
+            <h2 className="text-3xl md:text-4xl font-bold "
             >Garden Of Knowledge</h2>
             <div className='text-lg font-bold '>
-                <a href="/home" className='hover:text-cyan-500' >Home</a>
-                <a href="/libraryPrograms" className='pl-4 hover:text-cyan-500'>Library Programs</a>
-                <a href="/blogroll" className='pl-4 hover:text-cyan-500'>Blogroll</a>
-                <a href="/about" className='pl-4 hover:text-cyan-500'>About</a>
+                <a href="/home" className='hover:text-cyan-500 text-violet-800' >Home</a>
+                <a href="/libraryPrograms" className='pl-2 md:pl-4 hover:text-cyan-500 text-violet-800'>Library</a>
+                <a href="/blogroll" className='pl-2 md:pl-4 hover:text-cyan-500 text-violet-800'>Blogroll</a>
+                <a href="/about" className='pl-2 md:pl-4 hover:text-cyan-500 text-violet-800'>About</a>
             </div>
             </div>
             <hr className='mt-8'/>
